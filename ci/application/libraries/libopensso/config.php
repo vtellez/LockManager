c31d716e3c22b85e162f444a0bc51e29467e46bf
vtellez <tellez.victor@gmail.com>
2013-07-16 20:18:04 +0200

    gh-pages initial content

--- ci/application/libraries/libopensso/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configuration package used
-define('CONFIG_PACK', 'sso.us.es_INTERNO_001');
-
-
-// Package directory (do not change)
-define('PACK_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'packs' . DIRECTORY_SEPARATOR);
